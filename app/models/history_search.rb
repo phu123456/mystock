9ee4154af18947cd37f24f6f7a2df9a7a0c45fb7
Alongkorn <Slurpe3@gmail.com>
2017-10-04 09:01:46 +0700

    sd

--- app/models/history_search.rb
+++ app/models/history_search.rb
@@ -4,7 +4,7 @@ class HistorySearch
   def initialize(params)
     params ||= {}
     @date_from = parsed_date(params[:date_from], 30.day.ago.to_date.to_s)
-    @date_to = parsed_date(params[:date_to], Date.today.to_s)
+    @date_to = parsed_date(params[:date_to], Date.tomorrow.to_s)
   end
 
   def scope
