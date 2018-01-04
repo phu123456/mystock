bac7250076c14f00ae64a06fad53377a6dc418cd
Alongkorn <Slurpe3@gmail.com>
2018-01-04 09:12:25 +0700

    s

--- app/models/grease.rb
+++ app/models/grease.rb
@@ -1,11 +1,11 @@
 class Grease < ApplicationRecord
   def self.getExpire(date)
-    return (15.days.from_now.strftime("%F").to_date - date.strftime("%F").to_date).to_i
+    return (date.strftime("%F").to_date - Date.today.strftime("%F").to_date).to_i + 15
     # return (Date.today.to_date - date.strftime("%F").to_date).to_i
   end
 
   def self.getStatus(date)
-    expire_in = (15.days.from_now.strftime("%F").to_date - date.strftime("%F").to_date).to_i
+    expire_in = (date.strftime("%F").to_date - Date.today.strftime("%F").to_date).to_i + 15
     if expire_in < 2
       return "1 red"
     elsif expire_in < 5
